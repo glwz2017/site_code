b8ca8bba847b9df5fa82883b83d91e77aa7f9257
2991703821@qq.com <2991703821@qq.com>
2022-05-14 10:14:36 +0800

    vue3-demo-test

---
+++ vue3-demo/src/views/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h1> {{ msg }}</h1>
+    <p>
+      name: {{ person.name }}
+    </p>
+    <p>
+      age: {{ person.age }}
+    </p>
+    <button @click="changeNameFn">改变数据</button>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from 'vue'
+// @ is an alias to /src
+export default {
+  name: 'Index',
+  setup () {
+    const msg = ref('Welcome to Your Vue3.js App')
+    const person = reactive({
+      name: '张三',
+      age: 16
+    })
+
+    function changeNameFn () {
+      person.name = '格林威治'
+    }
+
+    return {
+      msg,
+      person,
+      changeNameFn
+    }
+  },
+  components: {}
+}
+</script>
